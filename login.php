bccd1336157d37bc08ee8bdff34c43c66ade5c0e
scientistmind <luk.ku@hotmail.com>
2020-06-26 22:39:12 +0530

    added login.php

---
+++ login.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<main>
+<div class="container">
+<div class="row mt-5">
+<div class="col-md-4 offset-4">
+<div class="card">
+<div class="card-header">
+<h3>Login Here</h3>
+</div>
+<div class="card-body">
+<form method='post'>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" name='user' id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" name='pass' id="exampleInputPassword1">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" name='submit' class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+<?php
+if(isset($_POST["submit"])){
+if(!empty($_POST['user']) && !empty($_POST['pass'])){
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$conn = new mysqli('localhost', 'root', '') or die(mysqli_error());
+$db = mysqli_select_db($conn, 'secsionregister') or die("databse error");
+$query = mysqli_query($conn, "SELECT * FROM userpass WHERE user='".$user."' AND pass='".$pass."'");
+$numrows = mysqli_num_rows($query);
+if($numrows !=0)
+{
+while($row = mysqli_fetch_assoc($query))
+{
+$dbusername=$row['user'];
+$dbpassword=$row['pass'];
+}
+if($user == $dbusername && $pass == $dbpassword)
+{
+session_start();
+$_SESSION['sess_user']=$user;
+header("Location:welcome.php");
+}
+}
+else
+{
+echo "Invalid Username or Password!";
+}
+}
+else
+{
+echo "Required All fields!";
+}
+}
+?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
